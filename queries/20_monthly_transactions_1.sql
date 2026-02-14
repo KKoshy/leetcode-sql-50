660ba0216b50e1d18c16a2b48ae796fce812c7c9
KKoshy <40890763+KKoshy@users.noreply.github.com>
2026-02-14 20:30:50 +0530

    Create 20_monthly_transactions_1.sql

---
+++ queries/20_monthly_transactions_1.sql
@@ -0,0 +1,10 @@
+SELECT
+    DATE_FORMAT(trans_date, '%Y-%m') AS month,
+    country,
+    COUNT(state) AS trans_count, 
+    COUNT(CASE WHEN state='approved' THEN 1 END) AS approved_count,
+    SUM(amount) AS trans_total_amount,
+    SUM(CASE WHEN state='approved' THEN amount END) AS approved_total_amount
+FROM
+    Transactions
+GROUP BY month, country;
